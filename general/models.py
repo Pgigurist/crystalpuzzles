e455e7b452ef87a13f00c0b539210814501573da
Pgigurist <pgigurist@gmail.com>
2019-12-25 22:42:17 +0300

    add bootstrap 4 to templates, create 'base' template, create static pages

--- general/models.py
+++ general/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=300)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+#    class Meta:
+#        verbose_name='News'
+#        verbose_names='News'
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
--- general/views.py
+++ general/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(req):
+    return render(req, 'general/index.html')
+
+def about(req):
+    return render(req, 'general/about.html')
