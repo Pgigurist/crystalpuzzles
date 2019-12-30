3726b8b49df4fe97fd39e413498a874bd4637e62
Pgigurist <pgigurist@gmail.com>
2019-12-30 21:04:28 +0300

    add Feedback form, but need to fix bootstrap api

--- general/models.py
+++ general/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Photo(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media/images/')
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     name = models.CharField(max_length=300)
     pub_date = models.DateTimeField(auto_now_add=True)
     text = models.TextField(blank=True)
+    image = models.ForeignKey(Photo, on_delete=models.CASCADE, blank=True, default=None)
 
     def __str__(self):
         return self.name
--- general/views.py
+++ general/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render
-
+from .models import Post, Photo, Feedback
+from .forms import FeedbackForm
 # Create your views here.
 
 def index(req):
-    return render(req, 'general/index.html')
+    context = {'posts' : Post.objects.all()[:3]}
+    return render(req, 'general/index.html', context)
 
 def about(req):
     return render(req, 'general/about.html')
+
+def news(req):
+    context = {'posts' : Post.objects.all()}
+    return render(req, 'general/news.html', context)
+
+def gallery(req):
+    context = {'photos' : Photo.objects.all()}
+    return render(req, 'general/gallery.html', context)
+
+def addFeedback(req):
+    """
+        добавляет левый отзыв/ fixed
+    """
+    posts = Feedback.objects.all()[:3]
+    
+    if req.method == 'POST':
+        form = FeedbackForm(req.POST)
+        if form.is_valid():
+            feedback = form.save()
+            print(feedback)
+            feedback.save()
+            return render(req, 'general/index.html')
+    else:
+        form = FeedbackForm()
+        context = {
+            'posts'  :   posts,
+            'feedback_form' : form,
+        }
+    return render(req,  'general/feedback.html', context)
+
+
+
