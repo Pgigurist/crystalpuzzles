97f0fd706d5c0e03ba76dbe3d9288b4d1783cb9e
Pgigurist <pgigurist@gmail.com>
2020-01-04 13:34:17 +0300

    add Group and Lesson models

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=300)
+    #image = models.ImageField(upload_to='media/images/')
+    time_start = models.TimeField(auto_now=False, auto_now_add=False)
+    time_end = models.TimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.title
+
+
