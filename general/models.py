f74faf2af6d92e922e55bdbf0423725f5fdff12c
Alexey Gubanov <pgigurist@gmail.com>
2020-01-08 13:05:24 +0300

    add moderate to feedback

--- general/models.py
+++ general/models.py
@@ -28,6 +28,7 @@ class Feedback(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     city = models.CharField(max_length=100)
     text = models.TextField(blank=True)
+    moderate = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
